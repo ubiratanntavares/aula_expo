3daae342f474d33da7a408ef2022475a0fbb63ac
ubiratanntavares <161661614+ubiratanntavares@users.noreply.github.com>
2024-03-20 22:45:29 -0300

    Touchable/function onPress
    Aprendemos a usar TouchableOpacity e function => onPress

--- App.js
+++ App.js
@@ -20,9 +20,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'orange',
-    //alignItems: 'center',
+    alignItems: 'center',
     justifyContent: 'center',
     padding:18,
-    
   },
 });
